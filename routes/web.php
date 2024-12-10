030f7cfbcdf920802c2dee6c55d296484527d4e3
ps <ps@gmail.com>
2024-12-10 15:19:45 +0530

    development v1

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/admin',function(){
+    return view('home');
+});
+
+Route::get('/home', function () {
+    return view('home');
 });
